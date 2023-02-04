9331a211f36e2aba3ca685597f6b91ec01b7379f
Byeongjun Moon <byeongjun@bjmoon.io>
2023-02-04 15:29:16 -0800

    editor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,92 +1,123 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '@/styles/Home.module.css'
+// import { OutputData } from "@editorjs/editorjs";
+// import type { NextPage } from "next";
+// import dynamic from "next/dynamic";
+// import { useState } from "react";
+// // important that we use dynamic loading here
+// // editorjs should only be rendered on the client side.
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Markysparky</title>
-        <meta name="description" content="HackSC '23 submission" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="main">
-        <div className="navbar">
-          <div className="navbar--title">
-            Markysparky
-          </div>
-          <div className='navbar--profile'>
+// export default function Home() {
+//   const EditorBlock = dynamic(() => import("../components/Editor"), {
+//     ssr: false,
+//   });
+//   const Home: NextPage = () => {
+//     //state to hold output data. we'll use this for rendering later
+//     const [data, setData] = useState<OutputData>();
+//   return (
+//     <>
+//       <Head>
+//         <title>Markysparky</title>
+//         <meta name="description" content="HackSC '23 submission" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       <div className="main">
+//         <div className="navbar">
+//           <div className="navbar--title">
+//             Markysparky
+//           </div>
+//           <div className='navbar--profile'>
             
-          </div>
-        </div>
-        <div className="body">
-          <div className="body--title">
-            😁 Positive
-          </div>
-          <div className="body--card--row">
-            <div className="body--card">
-            <div className="body--card--title">
-              Sample title
-            </div>
-            <div className="body--card--text">
-              Lorem ipsum hello holy shit i wanna sleep i&apos;m so fuckin tired.
-            </div>
+//           </div>
+//         </div>
+//         <div className="body">
+//           <div className="body--title">
+//             😁 Positive
+//           </div>
+//           <div className="body--card--row">
+//             <div className="body--card">
+//             <div className="body--card--title">
+//               Sample title
+//             </div>
+//             <div className="body--card--text">
+//               Lorem ipsum hello holy shit i wanna sleep i&apos;m so fuckin tired.
+//             </div>
 
-            <div className="body--card--subtitle">
-            😁 Positive
-            </div>
+//             <div className="body--card--subtitle">
+//             😁 Positive
+//             </div>
 
-            </div>
+//             </div>
 
-            <div className="body--card">
+//             <div className="body--card">
 
-            <div className="body--card--title">
-              Sample title
-            </div>
-            <div className="body--card--text">
-              Lorem ipsum hello holy shit i wanna sleep i&apos;m so fuckin tired.
-            </div>
+//             <div className="body--card--title">
+//               Sample title
+//             </div>
+//             <div className="body--card--text">
+//               Lorem ipsum hello holy shit i wanna sleep i&apos;m so fuckin tired.
+//             </div>
 
-            <div className="body--card--subtitle">
-            😁 Positive
-            </div>
+//             <div className="body--card--subtitle">
+//             😁 Positive
+//             </div>
 
-            </div>
-            <div className="body--card">
+//             </div>
+//             <div className="body--card">
 
-            <div className="body--card--title">
-              Sample title
-            </div>
-            <div className="body--card--text">
-              Lorem ipsum hello holy shit i wanna sleep i&apos;m so fuckin tired.
-            </div>
+//             <div className="body--card--title">
+//               Sample title
+//             </div>
+//             <div className="body--card--text">
+//               Lorem ipsum hello holy shit i wanna sleep i&apos;m so fuckin tired.
+//             </div>
 
-            <div className="body--card--subtitle">
-            😁 Positive
-            </div>
+//             <div className="body--card--subtitle">
+//             😁 Positive
+//             </div>
 
-            </div>
-            <div className="body--card">
+//             </div>
+//             <div className="body--card">
 
-            <div className="body--card--title">
-              Sample title
-            </div>
-            <div className="body--card--text">
-              Lorem ipsum hello holy shit i wanna sleep i&apos;m so fuckin tired.
-            </div>
+//             <div className="body--card--title">
+//               Sample title
+//             </div>
+//             <div className="body--card--text">
+//               Lorem ipsum hello holy shit i wanna sleep i&apos;m so fuckin tired.
+//             </div>
 
-            <div className="body--card--subtitle">
-            😁 Positive
-            </div>
+//             <div className="body--card--subtitle">
+//             😁 Positive
+//             </div>
 
-            </div>
-          </div>
-          
+//             </div>
+//           </div>
 
+//         <EditorBlock data={data} onChange={setData} holder="editorjs-container" />
 
-        </div>
-      </div>
-    </>
-  )
-}
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+// }
+
+import { OutputData } from "@editorjs/editorjs";
+import type { NextPage } from "next";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+// important that we use dynamic loading here
+// editorjs should only be rendered on the client side.
+const EditorBlock = dynamic(() => import("../components/Editor"), {
+  ssr: false,
+});
+const Home: NextPage = () => {
+  //state to hold output data. we'll use this for rendering later
+  const [data, setData] = useState<OutputData>();
+  return (
+    <EditorBlock data={data} onChange={setData} holder="editorjs-container" />
+  );
+};
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
+  plugins: [require("@tailwindcss/typography")],
+};
