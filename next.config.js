3bd1600bdcbab5a5e151b62d2b6c8435eb84481d
Byeongjun Moon <byeongjun@bjmoon.io>
2023-02-04 20:47:22 -0800

    flask server + next integration

--- next.config.js
+++ next.config.js
@@ -3,4 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: "/hello/:path*",
+        destination: "http://localhost:5000/hello/:path*",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+};
